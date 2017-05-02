74280812f506b29d73476c7ff7582f25dc72614d
ASanti <Darkstars31@gmail.com>
2017-05-02 00:15:50 -0500

    Added fontawesome for icons, particially implemented private messaging. Server is now more resilient to exceptions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.listen(3131);
 
 var uniqueAnimals = ['behomoth']
 var clientList = [];
-var clientChat = [];
+var clientChat = [""];
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -23,7 +23,7 @@ function handler (req, res) {
   });}
 
 
-
+console.log('NodeChat Server v-Alpha');
 console.log('Listening for connections');
 
 io.on('connection', function (socket) {
@@ -31,54 +31,69 @@ io.on('connection', function (socket) {
 	clientList.push(socket);
 	socket.on('initialize', function (data) {
 		console.log('New Connection with id: ' + socket.id);									  
-		updateClientList(socket);
-		updateClientChat(socket);
+		updateClientList(clientList);
+		//updateClientChat(clientList);
 	});		
 
-	socket.on('setUsername', function (data) {
-		socket.duplicateUsernameCount = clientList.map(function(client) {return client.name = data.username;}).length;	
+	socket.on('setUsername', function (data) {	
 		socket.name = data.username;
+		//socket.duplicateUsernameCount = clientList.map(function(client) {return client.name = data.username;}).length;			
 	});
 	
-
 	socket.on('disconnect', function() {
-      console.log('Client Disconnected ' + socket.id);
-      clientList.splice(clientList.indexOf(socket), 1);
-			updateClientList(socket);
+      	console.log('Client Disconnected ' + socket.id);
+      	clientList.splice(clientList.indexOf(socket), 1);
+		updateClientList(clientList);
    });
   
   socket.on('datain', function (data) {
 		if(data.input !== '' || null) {
 			if(data.input.startsWith("/")){
 					//Implement Help Functions
-			} else if (data.input.startsWith("@")) {
-					//Implement Direct Messages
+			} else if (data.input.startsWith("@")) {	
+				var input = data.input.substr(1);						
+				sendPrivateMessage(socket, input);
 			} else {
-				 console.log(socket.name +': '+ data.input);
-			//var name = (socket.duplicateUsernameCount > 0) ? socket.name+"("+socket.duplicateUsernameCount+")": socket.name; 
-			clientChat.push(socket.name +': '+ xssFilters.inHTMLData(data.input))
-			updateClientChat(socket);	
+				console.log(socket.name +': '+ data.input);
+				//var name = (socket.duplicateUsernameCount > 0) ? socket.name+"("+socket.duplicateUsernameCount+")": socket.name; 
+				var message = socket.name +': '+ xssFilters.inHTMLData(data.input);
+				clientChat.push(message);
+				updateClientChat(clientList, message);	
 			}
 		}
   });
 });
 
+function sendPrivateMessage (socket, input) {	
+	try {
+		var clients = findClientsByName(input.split(" ")[0]);
+		var message = socket.name +': '+ xssFilters.inHTMLData(input.substr(input.indexOf(' ')+1));
+		updateClientChat(clients, message);
+		// clients.forEach( function (client) {
+		// 	console.log('client: ',client);
+		// 	client.emit('updatePrivateMessage', {privateMessage: message});
+		// });
+	} catch ( e ) {
+		console.error("sendPrivateMessage Execption Message: " + e);
+	}
+}
+
 function findClientsByName (name) {
 	return clientList.map( function (client) {
-		return name === client.name;
-	})
+		if(name.toLowerCase() === client.name.toLowerCase()) {	return client; }	
+	});
 }
 
-function updateClientList (socket) {
-	clientList.forEach( function (client) {
+function updateClientList (clients) {
+	clients.forEach( function (client) {
 		client.emit('updateClientList', {clientIdList: aggregateClientIds()});
 	});
-		console.log(clientList.length + ' Clients Lists have been updated.');		
+	console.log(clientList.length + ' Clients Lists have been updated. ' + aggregateClientIds().join(","));		
 }
 
-function updateClientChat (socket) {
-	clientList.forEach( function (client) {
-		client.emit('updateClientChat', {clientChat: clientChat});
+function updateClientChat (clients, message) {
+	clients.forEach( function (client) {
+		client.emit('updateClientChat', {message: message});
 	});
 	console.log(clientList.length + ' Clients Chats have been updated.');	
 }
